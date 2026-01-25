0a0bd993080b9cd08f87b7875ec3f9e876947931
livinusonynso <livinusonyenso@gmail.com>
2026-01-25 23:19:37 +0100

    add throttling function implementation

---
+++ Debouncing_throttling/index.js
@@ -0,0 +1,12 @@
+const throttling = (func, limit) => {
+  let inThrottle;
+  return function() {
+    const args = arguments;
+    const context = this; 
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+};
